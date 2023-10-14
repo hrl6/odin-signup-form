e05f7cd6902b24ec04916b22b57ac2724dec810f
owo-del <123456789+trowom001@protonmail.com>
2023-10-14 09:22:57 +0800

    add focus, required, and invalid properties that work

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
-const inputs = document.querySelectorAll(`input`);
+const firstName = document.getElementById('user_firstname');
+const lastName = document.getElementById('user_lastname');
+const email = document.getElementById('user_email');
+const phone = document.getElementById('user_phonenum');
+const password = document.getElementById('user_pw');
+const confirmPassword = document.getElementById('user_confirmpw');
+const form = document.getElementById(`signupForm`);
 
-inputs.forEach(input => {
-    // input.addEventListener(`focus`, () => {
-    //     this.classList.remove(`error`);
-    // });
+const errorPW = document.getElementById(`errorpw`);
+const errorCPW = document.getElementById(`errorcpw`);
 
-    input.addEventListener(`input`, () => {
-        if (this.validity.valueMissing) {
-            this.classList.add(`error`);
-        } else {
-            this.classList.remove(`error`);
-        }
-    });
-});
+form.addEventListener(`submit`, (e) => {
+    e.preventDefault(); // prevent submission
+
+    let messagespw = [];
+    let messagescpw = [];
+
+    if(password.value.length <= 7){
+        messagespw.push(`Password must be at least 8 characters`);
+        errorPW.innerText = messagespw;
+    } else{
+        errorPW.innerText = '';
+    }
+
+    if(confirmPassword.value != password.value){
+        messagescpw.push(`Password not match`);
+        errorCPW.innerText = messagescpw;
+    } else{
+        errorCPW.innerText = '';
+    }
+
+    if(messages.length > 0){
+        e.preventDefault(); // prevent submission
+    }
+})
